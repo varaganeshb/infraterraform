06437aeabcff4180d83cf5ad32cd4e4c99f22a54
sasikanth53 <42739097+sasikanth53@users.noreply.github.com>
2022-10-21 13:47:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-2" 
+       default     = "ap-south-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "officelapkey" 
 }
 
 variable "instance_type" { 
