2b87580071ba4667dd36b013c7ba564abb9b8ea3
sasikanth53 <42739097+sasikanth53@users.noreply.github.com>
2022-10-21 14:01:26 +0530

    Updated AMI ID

--- sonar.tf
+++ sonar.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0b9064170e32bde34"
+      ami           = "ami-0e6329e222e662a52"
       key_name = var.key_name
       instance_type = "t2.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
