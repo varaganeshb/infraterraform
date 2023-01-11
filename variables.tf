e294b758bdf56ef1ab9304d46f4408a10276e9a4
sasikanth53 <42739097+sasikanth53@users.noreply.github.com>
2023-01-11 17:23:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "officelapkey" 
+    default     =  "HashTek-domain" 
 }
 
 variable "instance_type" { 
