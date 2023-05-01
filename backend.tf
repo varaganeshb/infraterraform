50d953202807b8b8d3a2d2ada785d4ce7e2b7016
varaganeshb <125376987+varaganeshb@users.noreply.github.com>
2023-05-01 16:48:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "hashtek-terraform-state-bucket"
+    bucket = "my-s3-project2"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "hashtekTerraformStatelock"
+    dynamodb_table = "mydynamo-project-2"
   }
 }
